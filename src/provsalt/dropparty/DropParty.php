a17fe0edabe68bd411d50ea81461c80f22bf4919
provsalt <39023494+provsalt@users.noreply.github.com>
2019-12-28 01:30:49 +0000

    Rewrite of config

--- src/provsalt/dropparty/DropParty.php
+++ src/provsalt/dropparty/DropParty.php
@@ -16,28 +16,7 @@ class DropParty extends PluginBase {
 	public $time;
 	
 	public function onEnable() {
-		$this->getLogger()->info("Has been enabled");
-		$this->cfg = (new Config($this->getDataFolder() . "config.yml", Config::YAML, array(
-		"World" => "world",
-		"Time" => 10,
-		"Duration" => 60,
-		"Message.Started" => "§a[§6Drop§bParty§a] §dHas started! Do /spawn to get the items at the §5DropParty",
-		"Message.Ended" => "§a[§6Drop§bParty§a] §dHas ended! Good game all.",
-		"Message.Countdown" => "§a[§6Drop§bParty§a] §dis starting in §5{time} §dminutes.",
-		"Popup.Enabled" => true,
-		"Popup.Message" => "§a[§6Drop§bParty§a] §dItems are dropping! §5Do /spawn to get those items!",
-		"Coordinates" => [
-		"X" => 0,
-		"Y" => 70,
-		"Z" => 0,
-		],
-		"Items" => [
-		57,
-		42,
-		22,
-		41,
-		],
-		)))->getAll();
+		$this->cfg = (new Config($this->getDataFolder() . "config.yml", Config::YAML)->getAll();
 		
 		$this->time = $this->cfg["Time"];
 		$this->getScheduler()->scheduleRepeatingTask(new task\DropPartyTask($this), 20 * 60);
